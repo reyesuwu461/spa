5b3a07c7200db5af4b8123e332d3afb715d16004
reyesuwu461 <samuelreyescastro456@gmail.com>
2025-07-06 13:10:07 -0500

    server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir archivos estáticos (incluyendo la carpeta 'game')
+// Serve static files (including the 'game' folder)
 app.use(express.static(path.join(__dirname)));
 app.use('/game', express.static(path.join(__dirname, 'game')));
 
+// Start the server
 app.listen(3000, () => {
-    console.log('Servidor en http://localhost:3000');
+    console.log('Server running at http://localhost:3000');
 });
